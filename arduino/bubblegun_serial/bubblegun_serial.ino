9f5ae2249041f6d4105c7347822df757e1f7574d
huynj316 <jhuynh2006@gmail.com>
2014-05-25 19:49:12 -0700

    Update bubblegun_serial.ino

--- arduino/bubblegun_serial/bubblegun_serial.ino
+++ arduino/bubblegun_serial/bubblegun_serial.ino
@@ -1,39 +1,59 @@
- 
-char inByte;         // incoming serial byte
-int bytesReceived[3];
-int count;
+ #include <Servo.h>
+Servo myservo;
 
-//activate motor
-//int motorVal = inByte[0];
-//analogWrite(motorPin, motorVal);
- 
+int pos = 0;
+int motorPin=9;
+
+int bytesReceived[3]; //incoming bytes
+
 void setup() {
-  // start serial port at 115200 bps:
+  pinMode(motorPin, OUTPUT); //motor on 9
+
+  myservo.attach(5); //servo on 5
   Serial.begin(9600);
-//  while (!Serial) {
-//    ; // wait for serial port to connect. Needed for Leonardo only
-//  }
 }
- 
+
 void loop()
 {
-  // if we get a valid byte, read analog ins:
-  if (Serial.available() > 0) { //we have received a request for messages
-  
-    // get incoming byte, in the future can use this for different commands
-    inByte = Serial.read();
-//    if(inByte==255){
-//     count = 0;
-//      bytesReceived[count]=inByte;
-//      count++;
-//    }else{
-      bytesReceived[count] = inByte;
-      count++;
- //   } 
-     
-     for(int i=0;i<3;i++){
-           Serial.println(bytesReceived[0]);   
-     }
+  if (Serial.available() >= 3) { //we have received THREE BYTES (at least)
+
+    for (int i=0; i< 3; i++){ //parse out all three bytes
+      bytesReceived[i] = Serial.read();
+    }
+
+    int motorStrength  = bytesReceived[0]; //use the first byte to determine which LED    
+    int whichMotor = bytesReceived[1]; //use the second byte to determine numBlinks
+    int blinkStrength     = bytesReceived[2]; //use the third to control blink speed (delay)
+    sendPower(motorStrength, whichMotor, blinkStrength); //pass that in to the blink function
   }
- 
 }
+
+
+void sendPower(int motorStrength, int whichMotor, int blinkStrength){ //blinkLED function
+ for(int i=0; i < numTimes; i++){
+  analogWrite(whichMotor, motorStrength);
+ }
+
+  if (bytesReceived[2] > 0) {
+    for(pos = 0; pos < 180; pos += 1)  // goes from 0 degrees to 180 degrees 
+    {                                  // in steps of 1 degree 
+      myservo.write(pos);              // tell servo to go to position in variable 'pos' 
+      delay(10);                       // waits 15ms for the servo to reach the position 
+    } 
+    for(pos = 180; pos>=1; pos-=1)     // goes from 180 degrees to 0 degrees 
+    {                                
+      myservo.write(pos);              // tell servo to go to position in variable 'pos' 
+      delay(5);                       // waits 15ms for the servo to reach the position 
+    } 
+    for(pos = 0; pos < 180; pos += 1)  // goes from 0 degrees to 180 degrees 
+    {                                  // in steps of 1 degree 
+      myservo.write(pos);              // tell servo to go to position in variable 'pos' 
+      delay(10);                       // waits 15ms for the servo to reach the position 
+    } 
+  }
+  delay(100); //put 1000 in between so we can see when 1 message starts and ends
+
+  //AT THE END OF THIS FUNCTION, I AM REQUESTING THE NEXT UPDATE FROM OF:
+  Serial.write('N');
+}
+
